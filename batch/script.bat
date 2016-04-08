375933ee24115ed9cd4f570c525894c0abf5fec9
Nils Laurent <nils.laurent@call4sport.com>
2016-04-08 11:47:33 +0200

    Added a simple bat script

---
+++ batch/script.bat
@@ -0,0 +1,49 @@
+:::::::::::::::::::::::::::::::::::::::::
+::
+:: Author : Nils Laurent
+:: Date : 08/04/2016
+::
+:::::::::::::::::::::::::::::::::::::::::
+
+@echo off
+
+:init
+echo.
+echo  - - - Initialization - - -
+
+goto :start
+
+:end
+exit
+
+
+:start
+
+echo.
+set /p myVar= Enter a short sentence : 
+
+echo.
+echo  All "a" will be replaced with "b"
+echo  %myVar%
+set myVar=%myVar:a=b%
+echo  %myVar%
+
+echo.
+pause
+
+echo.
+echo  Look how linux friendly we could be :
+
+echo  init = %~dp0babun-init.sh
+
+set init=%~dp0babun-init.sh
+set init=%init:C=c%
+set init=%init:\=/%
+set init=/%init::=%
+
+echo  init = %init%
+
+echo.
+pause
+
+goto :end
